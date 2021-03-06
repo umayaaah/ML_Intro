5f993e2b474375cb3ba9d6c3d69ab0f9c9597afb
Umayah Tahsin <umayah.tahsin@hotmail.co.uk>
2021-03-06 11:51:27 +0000

    Added DT classifier

--- CLF-Practice/main.py
+++ CLF-Practice/main.py
@@ -1,7 +1,9 @@
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture, output_image
-from NB_classwork import classify
+from NB_classwork import NB_classify
 from SVM_classwork import SVM_classify
+from DT_classwork import DT_classify
+
 
 import numpy as np
 import pylab as pl
@@ -19,11 +21,27 @@ bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if l
 
 # You will need to complete this function imported from the ClassifyNB script.
 # Be sure to change to that code tab to complete this quiz.
-clf = classify(features_train, labels_train, features_test, labels_test)
+def classifyNB():
+    # clf = classify(features_train, labels_train, features_test, labels_test)
 
-svm_clf = SVM_classify(features_train, labels_train, features_test, labels_test)
+    clf = NB_classify(features_train, labels_train, features_test, labels_test)
 
-### draw the decision boundary with the text points overlaid
-prettyPicture(svm_clf, features_test, labels_test)
-output_image("test.png", "png", open("test.png", "rb").read())
+    ### draw the decision boundary with the text points overlaid
+    prettyPicture(clf, features_test, labels_test)
+    output_image("test.png", "png", open("test.png", "rb").read())
 
+def classifySVM():
+    # clf = classify(features_train, labels_train, features_test, labels_test)
+
+    clf = SVM_classify(features_train, labels_train, features_test, labels_test)
+
+    ### draw the decision boundary with the text points overlaid
+    prettyPicture(clf, features_test, labels_test)
+    output_image("test.png", "png", open("test.png", "rb").read())
+
+def classifyDT(features_train, labels_train):
+    clf = DT_classify(features_train, labels_train)
+    prettyPicture(clf, features_test, labels_test)
+    output_image("tree.png", "png", open("test.png", "rb").read())
+
+classifyDT(features_train, labels_train)
