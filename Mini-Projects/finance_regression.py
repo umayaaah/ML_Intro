4d5a3a921f6a7213a4de81f28358feb01ccb0a46
Umayah Tahsin <umayah.tahsin@hotmail.co.uk>
2021-03-14 11:53:52 +0000

    regression clf fitted

--- Mini-Projects/finance_regression.py
+++ Mini-Projects/finance_regression.py
@@ -12,24 +12,26 @@ sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 dictionary = pickle.load( open("../tools/final_project/final_project_dataset_modified.pkl", "r") )
 
-### list the features you want to look at--first item in the 
-### list will be the "target" feature
+### list the features you want to look at--first item in the list will be the "target" feature
+### will be using salary to predict the bonus
 features_list = ["bonus", "salary"]
-data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
+data = featureFormat( dictionary, features_list, remove_any_zeroes=True, sort_keys = '../tools/python2_lesson06_keys.pkl')
 target, features = targetFeatureSplit( data )
 
 ### training-testing split needed in regression, just like classification
 from sklearn.model_selection import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
+### create and fit training set to regression
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg = reg.fit(feature_train, target_train)
 
-
-
-
-
+print("Slope: {0}".format(reg.coef_))
+print("Intercept: {0}".format(reg.intercept_))
 
 
 ### draw the scatterplot, with color-coded training and testing points
