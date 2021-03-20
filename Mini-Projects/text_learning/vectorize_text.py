9dffe6e841cc8112c805bcaf82ec05c657121097
Umayah Tahsin <umayah.tahsin@hotmail.co.uk>
2021-03-20 14:52:21 +0000

    applied preprocessing to email content

--- Mini-Projects/text_learning/vectorize_text.py
+++ Mini-Projects/text_learning/vectorize_text.py
@@ -29,45 +29,47 @@ from_chris = open("from_chris.txt", "r")
 from_data = []
 word_data = []
 
-### temp_counter is a way to speed up the development--there are
-### thousands of emails from Sara and Chris, so running over all of them
-### can take a long time
-### temp_counter helps you only look at the first 200 emails in the list so you
-### can iterate your modifications quicker
+### temp_counter limits the number of emails to iterate over
+### remove this limit to run over full dataset
 temp_counter = 0
 
 
 for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
-        ### once everything is working, remove this line to run over full dataset
         temp_counter += 1
         if temp_counter < 200:
             path = os.path.join('../../ud120-projects/', path[:-1])
-            print(path)
+            # print(path)
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
+            stemmed_email = parseOutText(email)
 
             ### use str.replace() to remove any instances of the words
-            ### ["sara", "shackleton", "chris", "germani"]
-
-            ### append the text to word_data
+            ignore_words = ["sara", "shackleton", "chris", "germani"]
+            reg = re.compile(r"\b%s\b|"%r"\b|\b".join(map(re.escape, ignore_words)))
+            stemmed_email = reg.sub("", stemmed_email)
+            
+            ### append the preprocessed text to word_data
+            word_data.append(stemmed_email)
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            from_data.append(name)
 
             email.close()
 
+def test():
+    print(word_data[152])
+    print(from_data[152])
+
+test()
 print("emails processed")
 from_sara.close()
 from_chris.close()
 
-pickle.dump( word_data, open("your_word_data.pkl", "w") )
-pickle.dump( from_data, open("your_email_authors.pkl", "w") )
-
-
-
+pickle.dump( word_data, open("your_word_data.pkl", "wb") )
+pickle.dump( from_data, open("your_email_authors.pkl", "wb") )
 
 
 ### in Part 4, do TfIdf vectorization here
